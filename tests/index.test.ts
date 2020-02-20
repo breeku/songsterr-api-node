23a13f160b7455951fad071b3e17aca42cfe5869
breeku <bregfi@hotmail.com>
2020-02-20 23:56:39 +0200

    added test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,7 @@
-it("index", () => {
-    expect.assertions(0)
+import { songsterrSearch } from "../src/index"
+
+it("returns array of objects", async () => {
+    expect.assertions(1)
+    const result = await songsterrSearch("Dyers eve")
+    expect(result).toBeDefined()
 })
--- tests/tuningToString.test.ts
+++ tests/tuningToString.test.ts
@@ -5,7 +5,6 @@ const exampleTuning = [64, 59, 55, 50, 45, 40]
 it("id array transforms to tuning object", () => {
     expect.assertions(1)
     const result = idToTuning(exampleTuning)
-    console.log(result)
     expect(result).toEqual({
         octave: "E2 A2 D3 G3 B3 E4",
         note: "e A D G B E",
